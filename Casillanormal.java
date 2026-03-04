7f3a8939fc01198b6665e88f42e87665a176c3c3
alvaromartinezmota <hugosanznavarro@alumnes.ilerna.com>
2026-03-04 19:00:52 +0100

    Creado casillanormal con codigo

---
+++ Casillanormal.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class CasillaNormal extends Casilla {
+
+    public CasillaNormal(int posicion) {
+        super(posicion);
+    }
+
+    @Override
+    public void activar(Pinguino jugador, Partida partida) {
+        // No hace nada
+    }
+}
